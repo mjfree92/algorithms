c44b688e0e603666e62f023e690327fe09a73a9b
mjfree <mjfree92@gmail.com>
2017-08-17 17:21:59 +0900

    start

---
+++ airport.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+void initialize();
+
+
+int main(){
+
+
+
+
+
+    return 0;
+}
+
+
+void initialize(int n){
+
+
+
+
+}
+
+
+int hasEdge(int u, int v){
+
+
+    
+
+
+
+
+    return whether
+}
